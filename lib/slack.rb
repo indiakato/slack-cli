af9698769e56661a0c088c1ad3991f9dd07d11f8
r-spiel <richelle.mehlhaff@gmail.com>
2020-10-06 16:31:09 -0700

    test printing resonse from users.list slack API

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,11 +1,22 @@
 #!/usr/bin/env ruby
 require_relative 'workspace'
+require 'httparty'
+require 'dotenv'
+Dotenv.load
 
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
 
+  base_url = "https://slack.com/api/users.list"
+  response = HTTParty.get(base_url, query: {
+      token: ENV["SLACK_TOKEN"]
+  }
+  )
+
+  pp response
   # TODO project
+
 
   puts "Thank you for using the Ada Slack CLI"
 end
