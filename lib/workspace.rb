376426ae4f9006252ee7efe3d88a37df5d04e346
India Kato <india.kato@gmail.com>
2020-10-07 16:42:52 -0700

    select user and select channel methods

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -13,4 +13,24 @@ class Workspace
     #table print?
   end
 
+  def select_user(input)
+    found_user = @users.find { |user| user.name == input }
+    if found_user
+      return found_user
+    else
+      found_user = @users.find { |user| user.slack_id == input }
+    end
+    return found_user
+  end
+
+  def select_channel(input)
+    found_channel = @channels.find { |channel| channel.name == input }
+    if found_channel
+      return found_channel
+    else
+      found_channel = @channels.find { |channel| channel.slack_id == input }
+    end
+    return found_channel
+  end
+
 end
