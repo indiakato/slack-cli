182a9a34c57833295e9ef4f05c67e9dcde1b4b85
India Kato <india.kato@gmail.com>
2020-10-09 10:26:13 -0700

    took out recipient variable and used workspace.selected instead

--- lib/slack.rb
+++ lib/slack.rb
@@ -11,32 +11,31 @@ def print_options
   puts "Your options are: \n1. List users \n2. List channels \n3. Select User \n4. Select Channel \n5. Details \n6. Send message \n7. Quit \n\nPlease enter the number of your choice: "
 end
 
-def select_user(workspace)
+def main_select_user(workspace)
   puts "Please enter the id or name of the user you seek:"
-  recipient = workspace.select_user(gets.chomp)
-  if recipient.nil?
+  workspace.select_user(gets.chomp)
+  if workspace.selected.nil?
     puts "User not found"
   else
-    puts "User selected: #{recipient.name}"
+    puts "User selected: #{workspace.selected.name}"
   end
-  return recipient
+  return workspace.selected
 end
 
-def select_channel(workspace)
+def main_select_channel(workspace)
   puts "Please enter the id or name of the channel you seek:"
-  recipient = workspace.select_channel(gets.chomp)
-  if recipient.nil?
+  workspace.select_channel(gets.chomp)
+  if workspace.selected.nil?
     puts "Channel not found"
   else
-    puts "Channel selected: #{recipient.name}"
+    puts "Channel selected: #{workspace.selected.name}"
   end
-  return recipient
+  return workspace.selected
 end
 
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
-  recipient = nil
   puts "There are #{workspace.channels.length} channels, and #{workspace.users.length} users."
 
   print_options
@@ -46,24 +45,20 @@ def main
     case choice
     when "1", "list users"
       tp workspace.users, :name, :slack_id, :real_name
-
-      # workspace.users.each { |user| puts "#{user.real_name} - status: #{user.status_text} - status emoji: #{user.status_emoji} user ID: #{user.slack_id} user-name: #{user.name}"}
     when "2", "list channels"
       tp workspace.channels, :name, :slack_id, :topic, :member_count
-
-      #workspace.channels.each { |channel| puts "Channel ID:#{channel.slack_id} - Name: #{channel.name} - Topic: #{channel.topic} - Number of members: #{channel.member_count}" }
     when "3", "select user"
-      recipient = select_user(workspace)
+      main_select_user(workspace)
     when "4", "select channel"
-      recipient = select_channel(workspace)
+      main_select_channel(workspace)
     when "5", "details"
-      if recipient.nil?
+      if workspace.selected.nil?
         puts "No user or channel selected"
       else
         puts workspace.show_details
       end
     when "6", "send message"
-      if recipient.nil? || recipient.instance_of?(User)
+      if workspace.selected.nil? || workspace.selected.instance_of?(User)
         puts "Invalid channel, you must have a channel selected"
       else
         puts "Please enter message to post:"
